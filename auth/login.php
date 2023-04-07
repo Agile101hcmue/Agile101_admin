2674a55563b07ef3a08857e1a2df4618a3a09aca
DesignContents <130153017+DesignContents@users.noreply.github.com>
2023-04-07 20:22:02 +0700

    OS-10: built UI for adding new product and promotions
    Tuyết and Hậu

--- auth/login.php
+++ auth/login.php
@@ -25,6 +25,8 @@
                     }
                 }
             }
+            // header("Location: ../index.php");
+            // die;
             echo "<script> 
                     alert('Sai thông tin đăng nhập quản trị')
                 </script>";
@@ -42,16 +44,61 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Agile101 - Xác thực quản trị</title>
+    <title>Xác thực quản trị</title>
+    <link rel="stylesheet" href="../bootstrap-5.0.2-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../bootstrap-5.0.2-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="../bootstrap-5.0.2-dist/css/all.min.css">
+    <script src="../bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
+    <script src="../bootstrap-5.0.2-dist/js/bootstrap.js"></script>
+    <!-- <link rel="stylesheet" href="./css/index.css"> -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@600&display=swap" rel="stylesheet">
+    
+
+    <link rel="stylesheet" href="../bootstrap_css/all.min.css">
+    <link rel="stylesheet" href="../bootstrap_css/bootstrap.min.css">
+    <link rel="stylesheet" href="../bootstrap_css/all.min.css">
+    <link rel="stylesheet" href="./css/login-form.css">
+    <script src="../bootstrap-5.0.2-dist/js/jquery.min.js"></script>
+    <script src="../bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
+    <script src="../bootstrap-5.0.2-dist/js/popper.min.js"></script>
+    <script src="../bootstrap-5.0.2-dist/js/all.min.js"></script>
 </head>
-<body>
-    <div align="center">
+
+<body class="text-center">
+    <div class="container text-center mt-5 ">
         <form action="" method="POST">
-            <h1 class="text-center">Login</h1>
-            <input type="text" name="admin_name" id="admin_name"><br>
-            <input type="password" name="password" id="password"><br>
-            <input type="submit" value="Login"><br>
+            <div class="text-center h3 fw-normal">
+                Xác thực
+            </div>
+            <small class="mb-5 "><i>Cần phải xác minh quyền quản trị trước khi thực hiện các thao tác quản lý</i></small>
+            <div class="d-flex justify-content-center mt-5">
+                <div class="form-floating mb-3 col-md-6">
+                    <label for="">Tên đăng nhập:</label>
+                    <input type="text" class="form-control mt-5"  name="admin_name" id="admin_name">
+                </div>
+            </div>
+            
+            <div class="d-flex justify-content-center">
+                <div class="form-floating mb-3 col-md-6">
+                    <label for="">Mật khẩu:</label>
+                    <input type="password" class="form-control mt-5" id="password"
+                        name="password">
+                </div>
+            </div>
+
+            <div>
+                <input class="btn btn-lg btn-primary mt-3 text-center" type="submit" value="Đăng nhập">
+            </div>
         </form>
+        <div class="mt-5">
+            <span>Chưa có tài khoản? <a href="./signup.php">Đăng ký ngay</a></span>
+        </div>
+        <div>
+            <a href="../" class="mt-4">Quay về trang chủ</a>
+        </div>
     </div>
+
 </body>
 </html>
--- components/edit.php
+++ components/edit.php
@@ -11,34 +11,132 @@
     <script src="../bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
     <script src="../bootstrap-5.0.2-dist/js/bootstrap.js"></script>
 </head>
-<h2>Edit Items</h2><br>
-<form action="crud.php" method="post" enctype="multipart/form-data">
-    <!-- edit form -->
-    <label for="">Name: </label> <input type="text" name="name" id="editname" required><br>
-    <label for="">Category: </label> <select name="category" id="category">
-        <option value="sach-giao-khoa">Sách giáo khoa</option>
-        <option value="truyen-tranh">Truyện tranh</option>
-        <option value="sach-giao-duc">Giáo dục</option>
-        <option value="tieu-thuyet">Tiểu thuyết</option>
-        <option value="loi-song">Lối sống & kỹ năng</option>
-        <option value="van-hoc-dan-gian">Văn học dân gian</option>
-        <option value="van-hoc-hien-dai">Văn học hiện đại</option>
-        <option value="sach-nuoc-ngoai">Tác phẩm nước ngoài</option>
-        <option value="hoc-thuat">Học thuật & báo cáo khoa học</option>
-        <option value="vu-tru">Vũ trụ & thiên văn</option>
-        <option value="ton-giao">Tôn giáo</option>
-        <option value="giao-trinh-dai-hoc">Các giáo trình cấp đại học</option>
-        <option value="luat">Luật & chính trị</option>
-        <option value="khac">Khác</option>
-    </select><br>
-    <label for="">Price: </label> <input type="number" name="price" id="editprice" min="100" required><br>
-    <label for="">Product Description: </label> <textarea name="desc" id="editdesc" cols="30" rows="10" required></textarea><br>
-    <img src="" id="editimg" width="100%" alt="">
-    <label for="">Image: </label> <input type="file" name="image" id="" accept=".jpg,.png,.jpeg"><br>
-    <label for="">Quantity: </label> <input type="number" name="quantity" min="1" id="editquantity"><br>
-    <input type="hidden" name="editpid" id="editpid">
-    <input type="submit" value="Confirm changes" name="editproduct">
-</form>
+<body class="text-center">
+<!-- <h2 align="center">Edit Items</h2><br> -->
+<!-- <div align="center"> -->
+    <!-- <form action="crud.php" method="post" enctype="multipart/form-data"> -->
+        <!-- edit form -->
+        <!-- <label for="">Name: </label> <input type="text" name="name" id="editname" required><br> -->
+        <!-- <label for="">Category: </label> <select name="category" id="category">
+            <option value="sach-giao-khoa">Sách giáo khoa</option>
+            <option value="truyen-tranh">Truyện tranh</option>
+            <option value="sach-giao-duc">Giáo dục</option>
+            <option value="tieu-thuyet">Tiểu thuyết</option>
+            <option value="loi-song">Lối sống & kỹ năng</option>
+            <option value="van-hoc-dan-gian">Văn học dân gian</option>
+            <option value="van-hoc-hien-dai">Văn học hiện đại</option>
+            <option value="sach-nuoc-ngoai">Tác phẩm nước ngoài</option>
+            <option value="hoc-thuat">Học thuật & báo cáo khoa học</option>
+            <option value="vu-tru">Vũ trụ & thiên văn</option>
+            <option value="ton-giao">Tôn giáo</option>
+            <option value="giao-trinh-dai-hoc">Các giáo trình cấp đại học</option>
+            <option value="luat">Luật & chính trị</option>
+            <option value="khac">Khác</option>
+        </select><br> -->
+        <!-- <label for="">Price: </label> <input type="number" name="price" id="editprice" min="100" required><br> -->
+        <!-- <label for="">Product Description: </label> <textarea name="desc" id="editdesc" cols="30" rows="10" required></textarea><br> -->
+        <!-- <img src="" id="editimg" width="100%" alt=""> -->
+        <!-- <label for="">Image: </label> <input type="file" name="image" id="" accept=".jpg,.png,.jpeg"><br> -->
+        <!-- <label for="">Quantity: </label> <input type="number" name="quantity" min="1" id="editquantity"><br> -->
+        <!-- <input type="hidden" name="editpid" id="editpid"> -->
+        <!-- <input type="submit" value="Confirm changes" name="editproduct"> -->
+    <!-- </form> -->
+
+    <div class="container text-center mt-5 ">
+        <form action="crud.php" method="post" enctype="multipart/form-data">
+            <div class="text-center h3 fw-normal">
+                Chỉnh sửa thông tin sản phẩm
+            </div>
+            <small class="mb-5 "><i>Các thông tin sẽ được cập nhật mới cho sản phẩm sau khi bạn nhấn Xác nhận</i></small>
+            <div class="d-flex justify-content-center mt-5">
+                <div class="form-floating mb-3 col-md-6">
+                    <label for="">Thay đổi tên sản phẩm:</label>
+                    <input type="text" class="form-control mt-5" name="name" id="editname">
+                </div>
+            </div>
+            
+            <div class="d-flex justify-content-center">
+                <div class="form-floating mb-3 col-md-6">
+                    <label for="">Thay đổi phân loại sản phẩm:</label>
+                    <select name="category" id="category" class="form-control mt-5">
+                        <option value="sach-giao-khoa">Sách giáo khoa</option>
+                        <option value="truyen-tranh">Truyện tranh</option>
+                        <option value="sach-giao-duc">Giáo dục</option>
+                        <option value="tieu-thuyet">Tiểu thuyết</option>
+                        <option value="loi-song">Lối sống & kỹ năng</option>
+                        <option value="van-hoc-dan-gian">Văn học dân gian</option>
+                        <option value="van-hoc-hien-dai">Văn học hiện đại</option>
+                        <option value="sach-nuoc-ngoai">Tác phẩm nước ngoài</option>
+                        <option value="hoc-thuat">Học thuật & báo cáo khoa học</option>
+                        <option value="vu-tru">Vũ trụ & thiên văn</option>
+                        <option value="ton-giao">Tôn giáo</option>
+                        <option value="giao-trinh-dai-hoc">Các giáo trình cấp đại học</option>
+                        <option value="luat">Luật & chính trị</option>
+                        <option value="khac">Khác</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="d-flex justify-content-center mt-5">
+                <div class="form-floating mb-3 col-md-6">
+                    <label for="">Cập nhật giá bán (VND):</label>
+                    <input type="number" name="price" id="editprice" min="100" class="form-control mt-5">
+                </div>
+            </div>
+
+            <div class="d-flex justify-content-center mt-5">
+                <div class="form-floating mb-3 col-md-6">
+                    <label for="">Cập nhật chi tiết sản phẩm:</label>
+                    <!-- <input type="text" class="form-control mt-5" name="name" id="editname"> -->
+                    <textarea name="desc" class="form-control mt-5" id="editdesc" cols="30" rows="10" required></textarea>
+                </div>
+            </div>
+
+            <!-- <img src="" id="editimg" width="100%" alt=""> review img -->
+            <!-- <label for="">Image: </label> br> -->
+
+            <div class="d-flex justify-content-center mt-5">
+                <div class="form-floating mb-3 col-md-6">
+                    <label for="">Cập nhật hình ảnh sản phẩm:</label>
+                    <input type="file" class="form-control mt-4" name="image" id="" accept=".jpg,.png,.jpeg">
+                    
+                    <!-- <input type="text" class="form-control mt-5" name="name" id="editname"> -->
+                </div>
+            </div>
+
+            <div class="d-flex justify-content-center mt-5">
+                <div class="form-floating mb-3 col-md-6">
+                <!-- <label for="">Quantity: </label> <input type="number" name="quantity" min="1" id="editquantity"><br> -->
+
+                    <label for="">Cập nhật tổng số lượng hiện có:</label>
+                    <input type="number" class="form-control mt-5"name="quantity" min="1" id="editquantity">
+                </div>
+            </div>
+
+            <input type="hidden" name="editpid" id="editpid">
+
+            <!-- <div class="d-flex justify-content-center mt-5">
+                <div class="form-floating mb-3 col-md-6">
+                    <label for="">Cập nhật tổng số lượng hiện có:</label>
+                    < <input type="submit" class="form-control mt-4" value="Xác nhận" "> -->
+                <!-- </div> -->
+            <!-- </div> -->
+            <!-- <input "> -->
+
+
+            <div>
+                <input type="submit" class="btn btn-lg btn-primary mt-3 text-center" value="Xác nhận" name="editproduct">
+            </div>
+        </form>
+        <div class="mt-5">
+            <span>Chưa có tài khoản? <a href="./signup.php">Đăng ký ngay</a></span>
+        </div>
+        <div>
+            <a href="../" class="mt-4">Quay về trang chủ</a>
+        </div>
+    </div>
+<!-- </div> -->
+</body>
 
 <?php
     include("../components/connection.php");
